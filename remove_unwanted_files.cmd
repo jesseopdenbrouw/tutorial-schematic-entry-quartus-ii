b67d9b63ba4e7d98e9e83eec2ded4875275b45bc
JESSE-PC\jesse <J.E.J.opdenBrouw@hhs.nl>
2018-09-17 19:11:01 +0200

    Windows script, first commit

---
+++ remove_unwanted_files.cmd
@@ -0,0 +1,43 @@
+@echo off
+rem
+rem
+echo.
+echo This program will delete all unnessecary files from Quartus projects.
+echo.
+echo Please be VERY carefull! Press Ctrl-C to break.
+echo.
+pause
+echo.
+
+echo Removing all db directories...
+FOR /D /r %%G in ("db*") DO rd /s/q %%G
+echo Removing all incremental_db directories...
+FOR /D /r %%G in ("incremental_db*") DO rd /s/q %%G
+echo Removing all simulations directories...
+FOR /D /r %%G in ("simulation*") DO rd /s/q %%G
+echo Removing all output_files directories...
+FOR /D /r %%G in ("output_files*") DO rd /s/q %%G
+
+del /s *.done
+del /s *.rpt
+del /s *.sof
+del /s *.pof
+del /s *.summary
+del /s *.jdi
+del /s vsim.wlf
+del /s *.bak
+del /s transcript
+del /s *assignment_defaults.qdf
+del /s *.pin
+del /s modelsim.ini
+del /s *.qws
+del /s *.smsg
+del /s *.map
+del /s *.cdf
+del /s *.dpf
+del /s PLLJ_PLLSPE_INFO.txt
+
+echo.
+echo Done.
+echo.
+pause
